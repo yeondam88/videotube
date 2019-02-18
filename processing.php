050e6ff01da80de9190f55fd674d9494df47ef27
Yeondam Park <giyen9088@gmail.com>
2019-02-17 17:46:51 -0800

    adding username when file is being uploaded

--- processing.php
+++ processing.php
@@ -4,8 +4,8 @@ require_once "includes/classes/VideoUploadData.php";
 require_once "includes/classes/VideoProcessor.php";
 
 if (!isset($_POST['uploadButton'])) {
-    echo 'No file sent to page.';
-    exit();
+	echo 'No file sent to page.';
+	exit();
 }
 
 // 1) Create file upload data
@@ -15,7 +15,7 @@ $videoUploadData = new VideoUploadData(
     $_POST['descriptionInput'],
     $_POST['privacyInput'],
     $_POST['categoryInput'],
-    "REPLACE-THIS"
+    $userLoggedInObj->getUsername()
 );
 // 2) Process video data (upload)
 $videoProcessor = new VideoProcessor($connection);
@@ -23,5 +23,5 @@ $wasSuccessful = $videoProcessor->upload($videoUploadData);
 // 3) Check if upload was successful
 
 if ($wasSuccessful) {
-    echo 'Upload successful';
+	echo 'Upload successful';
 }
