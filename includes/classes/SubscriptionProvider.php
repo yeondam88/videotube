06ef57a766e831100e611925482e81895e69c7e2
Yeondam Park <giyen9088@gmail.com>
2019-03-09 10:41:18 -0800

    getting subscribers

---
+++ includes/classes/SubscriptionProvider.php
@@ -0,0 +1,54 @@
+<?php
+class SubscriptionProvider
+{
+
+    private $connection;
+    private $userLoggedInObj;
+
+    public function __construct($connection, $userLoggedInObj)
+    {
+        $this->connection = $connection;
+        $this->userLoggedInObj = $userLoggedInObj;
+    }
+
+    public function getVideos()
+    {
+        $videos = array();
+        $subscriptions = $this->userLoggedInObj->getSubscriptions();
+
+        if (sizeof($subscriptions) > 0) {
+
+            $condition = "";
+            $i = 0;
+
+            while ($i < sizeof($subscriptions)) {
+                if ($i == 0) {
+                    $condition .= "WHERE uploadedBy=?";
+                } else {
+                    $condition .= " OR uploadedBy=?";
+                }
+                $i++;
+            }
+
+            $videoSql = "SELECT * FROM videos $condition ORDER BY uploadDate DESC";
+            $videoQuery = $this->connection->prepare($videoSql);
+
+            $i = 1;
+
+            foreach ($subscriptions as $sub) {
+                $videoQuery->bindParam($i, $subUsername);
+                $subUsername = $sub->getUsername();
+                $i++;
+            }
+
+            $videoQuery->execute();
+
+            while ($row = $videoQuery->fetch(PDO::FETCH_ASSOC)) {
+                $video = new Video($this->connection, $row, $this->userLoggedInObj);
+                array_push($videos, $video);
+            }
+        }
+
+        return $videos;
+    }
+}
