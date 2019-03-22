77e923718fb1856935d7e0e5e7f70188f7c2ed4e
Yeondam Park <giyen9088@gmail.com>
2019-03-21 22:40:06 -0700

    Adding trending

---
+++ includes/classes/TrendingProvider.php
@@ -0,0 +1,27 @@
+<?php
+class TrendingProvider
+{
+    private $connection;
+    private $userLoggedInObj;
+
+    public function __construct($connection, $userLoggedInObj)
+    {
+        $this->connection = $connection;
+        $this->userLoggedInObj = $userLoggedInObj;
+    }
+
+    public function getVideos()
+    {
+        $videos = array();
+
+        $query = $this->prepare("SELECT * FROM videos WHERE uploadDate >= now() - INTERVAL 7 DAY ORDER BY views DESC LIMIT 15");
+        $query->execute();
+
+        while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            $video = new Video($this->connection, $row, $this->userLoggedInObj);
+            array_push($videos, $video);
+        }
+
+        return $videos;
+    }
+}
